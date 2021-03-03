8770e1cde11abc0ea2608fb479b5eaca2bd35517
Satyam Chowdhury <satyam.chowdhury@gmail.com>
2021-03-03 12:04:47 +0530

    Add edit and delete functionality

--- src/main/java/org/communication/retry/controllers/MessageController.java
+++ src/main/java/org/communication/retry/controllers/MessageController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
@@ -38,6 +39,31 @@ public class MessageController {
             return "add-form";
         }
         messageRepository.save(message);
-        return "index";
+        return "redirect:/index";
     }
+
+    @GetMapping("/editForm/{id}")
+    public String showEditMessageForm(@PathVariable("id") long id, Model model){
+        Message message = messageRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid message id "+id));
+        model.addAttribute("message", message);
+        return "update-form";
+    }
+
+    @PostMapping("/update/{id}")
+    public String updateMessage(@PathVariable("id") long id, @Valid Message message, BindingResult result, Model model){
+        if(result.hasErrors()){
+            message.setId(id);
+            return "update-form";
+        }
+        messageRepository.save(message);
+        return "redirect:/index";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteMessage(@PathVariable("id") long id, Model model){
+        Message message = messageRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid message id "+id));
+        messageRepository.delete(message);
+        return "redirect:/index";
+    }
+
 }
